3c544aa76c5d26ec2be689a7c70c4026e152989e
Nagarjun P L <nagarjunpl4400@gmail.com>
2026-04-24 21:16:17 +0530

    Reduced the answers

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def chat(query: Query):
                     "role": "system",
                     "content": """You are an AI assistant for PES College of Engineering.
                     Answer clearly and completely using the given context.
-                    Answer only specific questions about PESCE, and say "I don't know" if the answer isn't in the context."""
+                    """
                 },
                 {
                     "role": "user",
